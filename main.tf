79aaad1cb5519f1c44f4b0061e1d77d8411cc8bd
pcsohany <127230937+pcsohany@users.noreply.github.com>
2023-03-30 18:08:22 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -2,17 +2,18 @@
 # SPDX-License-Identifier: MPL-2.0
 
 module "aft" {
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
-  ct_management_account_id    = var.ct_management_account_id
-  log_archive_account_id      = var.log_archive_account_id
-  audit_account_id            = var.audit_account_id
-  aft_management_account_id   = var.aft_management_account_id
-  ct_home_region              = var.ct_home_region
-  tf_backend_secondary_region = var.tf_backend_secondary_region
+   source                      = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+
+  ct_management_account_id    = var.911403190714
+  log_archive_account_id      = var.492604715259
+  audit_account_id            = var.159779466361
+  aft_management_account_id   = var.730506990486
+  ct_home_region              = var.eu-west-1
+  tf_backend_secondary_region = "eu-west-2"
 
   vcs_provider                                  = "github"
-  account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
-  account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
-  global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
-  account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+  account_request_repo_name                     = "${var.github_pcsohany}/learn-terraform-aft-account-request"
+  global_customizations_repo_name               = "${var.github_pcsohany}/learn-terraform-aft-global-customizations"
+  account_customizations_repo_name              = "${var.github_pcsohany}/learn-terraform-aft-account-customizations"
+  account_provisioning_customizations_repo_name = "${var.github_pcsohany}/learn-terraform-aft-account-provisioning-customizations"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,10 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-ct_management_account_id    = ""
-log_archive_account_id      = ""
-audit_account_id            = ""
-aft_management_account_id   = ""
-github_username             = ""
-ct_home_region              = ""
-tf_backend_secondary_region = ""
+ct_management_account_id    = "911403190714"
+log_archive_account_id      = "492604715259"
+audit_account_id            = "159779466361"
+aft_management_account_id   = "730506990486"
+github_username             = "pcsohany"
+ct_home_region              = "eu-west-1"
+tf_backend_secondary_region = "eu-west-2"
