ea67a31e8e3f7707cc7b47f583183fe8d07ba2b0
pcsohany <127230937+pcsohany@users.noreply.github.com>
2024-10-29 23:53:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@
 module "aft" {
    source                      = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
 
-  ct_management_account_id    = 911403190714
-  log_archive_account_id      = 492604715259
-  audit_account_id            = 159779466361
-  aft_management_account_id   = 730506990486
-  ct_home_region              = "eu-west-1"
-  tf_backend_secondary_region = "eu-west-2"
+  ct_management_account_id    = 123456789101
+  log_archive_account_id      = 123456789102
+  audit_account_id            = 123456789103
+  aft_management_account_id   = 123456789104
+  ct_home_region              = "eu-central-1"
+  tf_backend_secondary_region = "eu-central-2"
 
   vcs_provider                                  = "github"
   account_request_repo_name                     = "github.com/pcsohany/learn-terraform-aft-account-request"
